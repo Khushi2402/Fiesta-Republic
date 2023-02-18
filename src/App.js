489ae8cc86e02ec30635d29e71b569d1aa80df9a
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2023-02-18 18:26:09 +0530

    Changes made

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Countdown from './components/Countdown';
-import db from './firebase'
 
 function App() {
   return (
