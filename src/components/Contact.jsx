0769175adcd10a2b6fb3d954a5d75243507aefea
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2023-02-18 20:32:39 +0530

    Number changed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
       <div className='grid md:grid-cols-3 divide-x divide-black pt-10'>
         <div>
             <h1 className='text-xl font-normal pb-2'>PHONE</h1>
-            <p className='font-bold pb-6'>+91 7738123047</p>
+            <p className='font-bold pb-6'>+91 77558 86047</p>
         </div>
         <div>
             <h1 className='text-xl font-normal pb-2'>E-MAIL</h1>
@@ -23,7 +23,7 @@ const Contact = () => {
               <a href='https://twitter.com/FiestaRepublic?t=VeIxvIRC9bRSKdHOZi2MGQ&s=09'>
                 <BsTwitter className='px-2 w-10 h-10 cursor-pointer'/>
               </a>
-              <a href='https://wa.me/917738123047?text='>
+              <a href='https://wa.me/917755886047?text='>
                 <BsWhatsapp className='px-2 w-10 h-10 cursor-pointer'/>
               </a>
             </div>
@@ -35,7 +35,7 @@ const Contact = () => {
             <div className='flex flex-col items-center space-y-4'>
                 <h1 className='text-xl font-semibold'>Subscribe to our Newsletter</h1>
                 <p className=''>Subscribe to our Newsletter to get updates on all the exclusive events</p>
-                <input text='text' placeholder='Your email address' className='border-2 rounded-xl w-auto h-8 px-4'></input>
+                <input text='text' placeholder='Your email address' className='md border-2 rounded-xl w-auto px-2'></input>
                 <button className='rounded-xl'>Subscribe</button>
                 <p className='text-sm  underline underline-offset-auto hover:font-semibold cursor-pointer'>No thanks</p>
             </div>
